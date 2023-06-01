82b8d766af2ca273cb417780418d78fe44f8946c
LAG13_OFC <luka17sas@gmail.com>
2023-06-01 00:03:36 -0300

    sfdfs

--- install.sh
+++ install.sh
@@ -10,8 +10,21 @@ wget https://raw.githubusercontent.com/LAG13-OFC/script-bash/master/psi.sh -O "$
 # Dar permisos de ejecución al archivo
 chmod +x "$install_dir/psi.sh"
 
-# Agregar el directorio de instalación al PATH del usuario actual
-echo "export PATH=\"$PWD/$install_dir:\$PATH\"" >> ~/.bashrc
-source ~/.bashrc
+# Obtener el directorio del archivo de configuración de bash
+bash_config_file=
+if [ -f "$HOME/.bashrc" ]; then
+    bash_config_file="$HOME/.bashrc"
+elif [ -f "$HOME/.bash_profile" ]; then
+    bash_config_file="$HOME/.bash_profile"
+fi
+
+# Agregar el directorio de instalación al PATH del usuario actual en el archivo de configuración de bash
+if [ -n "$bash_config_file" ]; then
+    echo "export PATH=\"$PWD/$install_dir:\$PATH\"" >> "$bash_config_file"
+    source "$bash_config_file"
+    echo "El directorio de instalación se ha agregado al PATH del usuario actual."
+else
+    echo "No se encontró el archivo de configuración de bash (.bashrc o .bash_profile). Asegúrate de agregar manualmente \"$PWD/$install_dir\" al PATH."
+fi
 
 echo "Instalación completada. Ahora puedes usar el comando 'psi' para acceder al panel de instalación."
