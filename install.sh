842953e092a7fee1a5dd4b4f9a2f06291754333c
LAG13_OFC <luka17sas@gmail.com>
2023-05-31 22:13:28 -0300

    d

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Establecer la ubicación de instalación
-install_dir="/ruta/de/instalacion"
+install_dir="/psi"
 
 # Clonar el repositorio de GitHub en el directorio de instalación
-git clone https://github.com/usuario/repo.git "$install_dir"
+git clone https://raw.githubusercontent.com/LAG13-OFC/script-bash/master/psi.sh "$install_dir"
 
 # Crear un enlace simbólico para el comando "psi"
-sudo ln -s "$install_dir/panel_instalacion.sh" /usr/local/bin/psi
+sudo ln -s "$install_dir/psi.sh" /usr/local/bin/psi
 
 echo "Instalación completada. Ahora puedes usar el comando 'psi' para acceder al panel de instalación."
