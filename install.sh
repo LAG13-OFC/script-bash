463df76cfdc7acabb7190bc0b6dd3950c609a721
LAG13_OFC <luka17sas@gmail.com>
2023-05-31 23:16:45 -0300

    ajustes

--- install.sh
+++ install.sh
@@ -3,8 +3,11 @@
 # Establecer la ubicación de instalación
 install_dir="/psi"
 
-# Clonar el repositorio de GitHub en el directorio de instalación
-git clone https://raw.githubusercontent.com/LAG13-OFC/script-bash/master/psi.sh "$install_dir"
+# Descargar el archivo psi.sh desde la URL
+wget https://raw.githubusercontent.com/LAG13-OFC/script-bash/master/psi.sh -O "$install_dir/psi.sh"
+
+# Dar permisos de ejecución al archivo
+chmod +x "$install_dir/psi.sh"
 
 # Crear un enlace simbólico para el comando "psi"
 sudo ln -s "$install_dir/psi.sh" /usr/local/bin/psi
