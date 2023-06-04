fb788460024661e749c52f19a016704b0e6354f6
LAG13_OFC <luka17sas@gmail.com>
2023-06-04 13:10:50 -0300

    ajustes 3

--- install.sh
+++ install.sh
@@ -21,10 +21,11 @@ fi
 # Agregar el directorio de instalación al PATH del usuario actual en el archivo de configuración de bash
 if [ -n "$bash_config_file" ]; then
     echo "export PATH=\"$PWD/$install_dir:\$PATH\"" >> "$bash_config_file"
+    echo "alias psi=\"$PWD/$install_dir/psi.sh\"" >> "$bash_config_file" # Agrega esta línea para crear el alias
     source "$bash_config_file"
     echo "El directorio de instalación se ha agregado al PATH del usuario actual."
 else
-    echo "No se encontró el archivo de configuración de bash (.bashrc o .bash_profile). Asegúrate de agregar manualmente \"$PWD/$install_dir\" al PATH."
+    echo "No se encontró el archivo de configuración de bash (.bashrc o .bash_profile). Asegúrate de agregar manualmente \"$PWD/$install_dir\" al PATH y crear un alias para 'psi.sh'."
 fi
 
 echo "Instalación completada. Ahora puedes usar el comando 'psi' para acceder al panel de instalación."
